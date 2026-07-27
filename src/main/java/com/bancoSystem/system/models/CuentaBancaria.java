74dc6d693d2a8026c5f6b567176067e31a75b17e
Mtrjoel <monterojoel135@gmail.com>
2026-07-27 19:34:49 -0400

    Refactor CuentaBancaria model to include balance and estado fields with appropriate getters

--- src/main/java/com/bancoSystem/system/models/CuentaBancaria.java
+++ src/main/java/com/bancoSystem/system/models/CuentaBancaria.java
@@ -1,18 +1,53 @@
 package com.bancoSystem.system.models;
 
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "cuentaBancaria")
 public class CuentaBancaria {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
+    @Column(unique = true, nullable = false)
     private String numeroCuenta;
     private String titular;
+    private BigDecimal balance;
+
+    public enum Estados {
+        ACTIVA, BLOQUEADA, CANCELADA
+    }
+
+    private Estados estado;
+
+    // ! getter y setters
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getNumeroCuenta(){
+        return numeroCuenta;
+    }
+
+    public String getTitular(){
+        return titular;
+    }
+
+    public BigDecimal getBalance(){
+        return balance;
+    }
+
+    public Estados getEstados(){
+        return estado;
+    }
 }
