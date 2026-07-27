9f2ef6e89fb5b10fcca196d1f212a489422ddebd
Mtrjoel <monterojoel135@gmail.com>
2026-07-27 19:44:15 -0400

    Refactor CuentaBancaria model to improve field annotations and update getter/setter methods

--- src/main/java/com/bancoSystem/system/models/CuentaBancaria.java
+++ src/main/java/com/bancoSystem/system/models/CuentaBancaria.java
@@ -5,6 +5,8 @@ import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,16 +22,22 @@ public class CuentaBancaria {
     
     @Column(unique = true, nullable = false)
     private String numeroCuenta;
+
+    @Column(nullable = false)
     private String titular;
+
+    @Column(nullable = false)
     private BigDecimal balance;
 
     public enum Estados {
         ACTIVA, BLOQUEADA, CANCELADA
     }
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Estados estado;
 
-    // ! getter y setters
+    // ! getters
 
     public Long getId(){
         return id;
@@ -47,7 +55,25 @@ public class CuentaBancaria {
         return balance;
     }
 
-    public Estados getEstados(){
+    public Estados getEstado(){
         return estado;
+    }
+
+    // ! Setters
+
+    public void setNumeroCuenta(String numeroCuenta){
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public void setTitular(String titular){
+        this.titular = titular;
+    }
+
+    public void setBalance(BigDecimal balance){
+        this.balance = balance;
+    }
+
+    public void setEstado(Estados estado){
+        this.estado = estado;
     }
 }
